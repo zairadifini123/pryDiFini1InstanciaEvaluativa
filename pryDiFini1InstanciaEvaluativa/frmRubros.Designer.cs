5455f4ed72825aca3e172b90bd6109e2cd77fa57
zairadifini <zairadifini123@gmail.com>
2026-05-05 12:29:40 -0300

    Agregar archivos de proyecto.

---
+++ pryDiFini1InstanciaEvaluativa/frmRubros.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace pryDiFini1InstanciaEvaluativa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            cmbRubros = new ComboBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(166, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Elige un rubro";
+            // 
+            // cmbRubros
+            // 
+            cmbRubros.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbRubros.FormattingEnabled = true;
+            cmbRubros.Location = new Point(184, 39);
+            cmbRubros.Name = "cmbRubros";
+            cmbRubros.Size = new Size(151, 28);
+            cmbRubros.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(cmbRubros);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Rubros ";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox cmbRubros;
+    }
+}
---
+++ pryDiFini1InstanciaEvaluativa/frmRubros.cs
@@ -0,0 +1,10 @@
+namespace pryDiFini1InstanciaEvaluativa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
